ba3df4828805fcd7231f89fafcadee7a38be38e2
DerpyDoggo11 <vrs18451@gmail.com>
2026-02-28 13:04:15 -0800

    added player movement

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,25 +1,16 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@export var speed: float = 100.0
+@export var acceleration: float = 50.0
+@export var friction: float = 30.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var direction = Input.get_vector("left", "right", "up", "down")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction != Vector2.ZERO:
+		velocity = velocity.lerp(direction * speed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = velocity.lerp(Vector2.ZERO, friction * delta)
+	
+	
 	move_and_slide()
