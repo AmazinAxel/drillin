73a08e98d72bbd7e26686acc3ea7b07d71f53675
DerpyDoggo11 <vrs18451@gmail.com>
2026-03-01 09:53:21 -0800

    sync

--- scenes/Player/playerMovement.gd
+++ scenes/Player/playerMovement.gd
@@ -3,9 +3,16 @@ extends CharacterBody2D
 const SPEED = 10
 const JUMP_VELOCITY = -350.0
 
+@export var isAttacking: bool = false
+
 @onready var health_bar = $TextureProgressBar
 @onready var sprite = $AnimatedSprite2D
-@onready var damageParticles = $damageParticles
+
+@onready var ohParticles = $ohParticles
+@onready var ouchParticles = $ouchParticles
+@onready var uhParticles = $uhParticles
+@onready var unfriendlyOuchParticles = $unfriendlyOuchParticles
+
 @onready var walkingParticles = $walkingParticles
 @onready var flashlight = $flashlight
 
@@ -31,7 +38,9 @@ func _physics_process(delta: float) -> void:
 		flashlight.rotation = lightDirection
 		
 		sprite.play("default")
-		walkingParticles.emitting = true
+		
+		if is_on_floor():
+			walkingParticles.emitting = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		sprite.set_frame_and_progress(0, 0)
@@ -49,7 +58,18 @@ func take_damage(amount):
 	var actual_damage = amount * Globals.damageReduction
 	Globals.health -= actual_damage
 	Globals.health = max(Globals.health, 0)
-	damageParticles.emitting = true
+	
+	var rand_int = randi_range(1, 4)
+	
+	if (rand_int == 1):
+		ohParticles.emitting = true
+	elif (rand_int == 2):
+		uhParticles.emitting = true
+	elif (rand_int == 3):
+		ouchParticles.emitting = true
+	elif (rand_int == 4):
+		unfriendlyOuchParticles.emitting = true
+		
 	if health_bar:
 		health_bar.value = Globals.health
 	modulate = Color.RED
@@ -62,4 +82,22 @@ func take_damage(amount):
 		Globals.damageReduction = 1;
 		Globals.shootSpeed = 1;
 
-		
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack") and not isAttacking:
+		attack()
+
+func attack():
+	if (self.name == "Player"):
+		#var manager = get_node("/root/main/GameManager")
+		#manager.lastDamageReason = "attack"
+		#manager.health -= 5
+		isAttacking = true
+		#self.get_node("DamageSound").play()
+		var timer = get_node("attackDelay");
+		timer.start()
+
+
+func _on_attack_delay_timeout() -> void:
+	isAttacking = false 
+	#var attackSprite = get_node("hurtBox/CollisionShape2D/Sprite2D")
+	#attackSprite.visible = false
