d16cdb00c09aee9397fb3ba397e3cb95eb9d0995
DerpyDoggo11 <vrs18451@gmail.com>
2026-02-28 18:44:59 -0800

    added drill menu and added animation

--- scenes/Drilla/drillMenu.gd
+++ scenes/Drilla/drillMenu.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	print(gui)
 	print(area)
 	print(player)
-	print(drillShop)
+	play("default")
 	
 var trigger_distance := 50.0  # pixels
 
@@ -21,5 +21,7 @@ func _process(delta: float) -> void:
 	
 	if dist < trigger_distance:
 		gui.visible = true
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://scenes/UI/DrillaShop.tscn");
 	else:
 		gui.visible = false
---
+++ scenes/UI/upgradeShop.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_damage_upgrade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_health_upgrade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_risk_upgrade_pressed() -> void:
+	pass # Replace with function body.
