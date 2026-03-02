c2a006623403f33c3d4fe9698a2c5e199ca38b6e
DerpyDoggo11 <vrs18451@gmail.com>
2026-03-01 16:38:05 -0800

    sync

--- scenes/BigDrillaProjectile/bigDrillaProjectile.gd
+++ scenes/BigDrillaProjectile/bigDrillaProjectile.gd
@@ -2,26 +2,35 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 var player: Node2D
-var returning := false
 var direction: Vector2 = Vector2.RIGHT
-
+var homing: bool = false
 
 func launch(dir: Vector2):
-	direction = dir
-	rotation = dir.angle()
+	rotation = direction.angle()
+	
+	await get_tree().create_timer(0.5).timeout 
+	if not is_instance_valid(self):
+		return
+	homing = false
 
 func _physics_process(delta):
-	velocity = direction * SPEED
-	var collision = move_and_slide()
+	if homing:
+		var p = get_tree().get_first_node_in_group("player")
+		if p:
+			direction = direction.lerp((p.global_position - global_position).normalized(), delta * 8).normalized()
+			rotation = direction.angle()
 	
-	await get_tree().create_timer(5.0).timeout
+	velocity = direction * SPEED
+	move_and_slide()
+
+func _ready():
+	await get_tree().create_timer(3.0).timeout
 	if is_instance_valid(self):
 		queue_free()
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	var player = get_tree().get_first_node_in_group("player")
 	if body.name == "Player":
-		if player:
-			player.take_damage(10)
-			queue_free()
+		var p = get_tree().get_first_node_in_group("player")
+		if p:
+			p.take_damage(10)
+		queue_free()
