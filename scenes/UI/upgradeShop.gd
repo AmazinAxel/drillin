0f77fd0fddc2df1216b885442c538d0a3b2087a4
AmazinAxel <axel@amazinaxel.com>
2026-03-04 19:54:11 -0800

    fix death animation

--- scenes/UI/upgradeShop.gd
+++ scenes/UI/upgradeShop.gd
@@ -1,22 +1,43 @@
 extends Control
 
+func _ready():
+	whatCanBuy()
+
 func _on_damage_upgrade_pressed() -> void:
 	if Globals.minerals >= 3:
 		Globals.minerals -= 3
 		Globals.attackDamage += 1
 		$purchase.play()
+		whatCanBuy()
 
 func _on_health_upgrade_pressed() -> void:
 	if Globals.minerals >= 3:
 		Globals.minerals -= 3
-		Globals.damageReduction -= 0.3  # takes less damage (since damage = amount * damageReduction)
+		Globals.damageReduction -= 0.3
 		$purchase.play()
+		whatCanBuy()
 
 func _on_replenish_health_pressed() -> void:
 	if Globals.minerals >= 1:
 		Globals.minerals -= 1
 		Globals.health = 100
 		$purchase.play()
+		whatCanBuy()
 
 func _process(delta):
 	$MineralsLabel.text = str(Globals.minerals)
+
+func whatCanBuy():
+	var upgradeWeapon = $HBoxContainer/VBoxContainer/upgradeWeapon
+	var upgradeArmor = $HBoxContainer/VBoxContainer/upgradeArmor
+	var replenishHealth = $HBoxContainer/VBoxContainer/ReplenishHealth
+
+	if Globals.minerals > 0:
+		if Globals.health > 80:
+			replenishHealth.disabled = true
+	else:
+		replenishHealth.disabled = true
+
+	if Globals.minerals < 3:
+		upgradeWeapon.disabled = true
+		upgradeArmor.disabled = true
--- scenes/globals.gd
+++ scenes/globals.gd
@@ -7,3 +7,21 @@ var shootSpeed = 1;
 var attackDamage = 2;
 var minerals = 0;
 var riskChance = 10;
+
+var isAttacking: bool = false;
+
+var baseRotation = 0;
+
+var isDead := false;
+
+
+func screen_shake(strength: float, duration: float):
+	var camera = get_viewport().get_camera_2d()
+	if not camera:
+		return # could prevent errors idk
+
+	var tween = create_tween()
+	tween.tween_method(func(t):
+		camera.offset = Vector2(randf_range(-strength, strength), randf_range(-strength, strength))
+	, 0.0, 1.0, duration)
+	tween.tween_callback(func(): camera.offset = Vector2.ZERO)
